df3a1c6fdb4ff4baf85ad2c230026dc1ee07c53b
Raddames <raddamestonui48@gmail.com>
2024-06-29 16:49:06 +0300

    first commit

---
+++ server/models.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# Initialize the SQLAlchemy object
+db = SQLAlchemy()
+
+# Define the Students model
+class Students(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+
+    # Relationship to Course model
+    # 'lazy=True' defers the loading of related Course objects until the courses attribute is accessed
+    courses = db.relationship('Course', backref='student', lazy=True)
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    code = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+
+    # Foreign key to reference Students table
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
